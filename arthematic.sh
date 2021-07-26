42c22386fe1ee943e7884068e882cd79c494f736
Abul Hassan <mail@wathingeyes.in>
2021-07-26 22:21:01 +0530

    Commiting one File arthematic.sh

---
+++ arthematic.sh
@@ -0,0 +1,22 @@
+#Purpose: Arthemetic Operators 
+#Version:1.5
+#Created Date: Sunday 11 July 2021 10:05:37 AM IST
+#Modified Date:
+#Author: Watching Eyes
+# START #
+
+echo "Please enter value 1: \c"
+read -r a 
+echo "Please enter value 2: \c"
+read -r b
+
+echo "a+b value is $(($a+b))"
+echo "a-b value is $(($a-b))"
+echo "a*b value is $(($a*b))"
+echo "a/b value is $(($a/b))"
+echo "a%b value is $(($a%b))"
+
+echo "Process Completed Successfully"
+
+
+# END #
